d79c3fc603a6282f14f668880c6c75a8e4805480
Leesuhuck <70427087+Leesuhuck@users.noreply.github.com>
2020-10-26 02:01:42 +0900

    Update Java_Calculator_demo.java

--- Java_Calculator_demo.java
+++ Java_Calculator_demo.java
@@ -1,43 +1,260 @@
-import java.util.Scanner;
-//출처 : https://stackoverflow.com/users/5766201/chandweer
-// 자바 계산기 알고리즘 데모용
-public class Java_Calculator_demo {
-public static void main(String[] args) {
 
-    Scanner in = new Scanner(System.in);
-    
-    char operation;
-    int num1;
-    int num2;
-    
-     System.out.println("Enter First Number");
-    
-    num1 = in.nextInt();
-    
-    System.out.println("Enter Operation");
-    
-    operation = in.next().charAt(0);
-    
-    System.out.println("Enter Second Number");
-    
-    num2 = in.nextInt();
-    
-    if (operation == '+')//make sure single quotes
-    {
-        System.out.println("your answer is " + (num1 + num2));
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.util.*;
+
+public class Java_Calculator_demo extends JFrame {
+/* extends : 상속
+부모 메서드를 그대로 사용할수도 있고 오버라이딩 할 필요없이 부모에 구현되있는것을 직접 사용가능
+Java_Calulator_demo는 하나에 공용클래스이며 상속 JFrame 클래스가 된다.
+*/
+    private JButton jbtNum1;
+    // private 클래스내에서만 접근이 가능한 접근제어자 
+    private JButton jbtNum2;
+    private JButton jbtNum3;
+    private JButton jbtNum4;
+    private JButton jbtNum5;
+    private JButton jbtNum6;
+    private JButton jbtNum7;
+    private JButton jbtNum8;
+    private JButton jbtNum9;
+    private JButton jbtNum0;
+    private JButton jbtAdd;
+    //더하기
+    private JButton jbtSubtract;
+    //뺴기
+    private JButton jbtMultiply;
+    //곱하기
+    private JButton jbtDivide;
+    //나누기
+    private JButton jbtSolve;
+    //풀다
+    private JButton jbtClear;
+    //초기화
+    private double TEMP;
+    /*double: 실수표현 (float와 다르게 소수뒤에f를 추가를 안해도 되게 사용한듯 하다)
+    이러한 부동 소수점에 단점은 가수부분을 이진수의 소수로 사용하기 때문에 1/2 1/4 ... 표현하지 못하는
+    소수가 존재하게 되는 것. ex) value = 0.021000058
+    */
+    private double SolveTEMP;
+    //템프를풀다
+    private JTextField jtfResult;
+    //결과
+
+    Boolean addBool = false;
+    Boolean subBool = false;
+    Boolean divBool = false;
+    Boolean mulBool = false;
+    //이걸 왜 다 거짓으로?
+    String display = "";
+    //이건 뭐지?
+    public Java_Calculator_demo() {
+// 자식 클래스
+        JPanel p1 = new JPanel();
+        p1.setLayout(new GridLayout(4, 3));
+        /* 
+        원하는 형과 열로 구성된 레이아웃을 작성할때 Grid Layout을 사용한다.
+        이와 같은 것은 JFrame(도화지) 안에 Layout(색종이) JPanel(색종이를 붙이기위한 종이)
+        JFrame>JPanel>JLayout
+        하지만 Layout과 Panel은 따로다. 그것을 주의하면서 이해
+        */
+        p1.add(jbtNum1 = new JButton("1"));
+        p1.add(jbtNum2 = new JButton("2"));
+        p1.add(jbtNum3 = new JButton("3"));
+        p1.add(jbtNum4 = new JButton("4"));
+        p1.add(jbtNum5 = new JButton("5"));
+        p1.add(jbtNum6 = new JButton("6"));
+        p1.add(jbtNum7 = new JButton("7"));
+        p1.add(jbtNum8 = new JButton("8"));
+        p1.add(jbtNum9 = new JButton("9"));
+        p1.add(jbtNum0 = new JButton("0"));
+        p1.add(jbtClear = new JButton("C"));
+        // JFrame 안에 JButton을 GUI창에 추가
+        JPanel p2 = new JPanel();
+        p2.setLayout(new FlowLayout());
+        p2.add(jtfResult = new JTextField(20));
+        jtfResult.setHorizontalAlignment(JTextField.RIGHT);
+        jtfResult.setEditable(false);
+
+        JPanel p3 = new JPanel();
+        p3.setLayout(new GridLayout(5, 1));
+        p3.add(jbtAdd = new JButton("+"));
+        p3.add(jbtSubtract = new JButton("-"));
+        p3.add(jbtMultiply = new JButton("*"));
+        p3.add(jbtDivide = new JButton("/"));
+        p3.add(jbtSolve = new JButton("="));
+
+        JPanel p = new JPanel();
+        p.setLayout(new GridLayout());
+        p.add(p2, BorderLayout.NORTH);
+        p.add(p1, BorderLayout.SOUTH);
+        p.add(p3, BorderLayout.EAST);
+
+        add(p);
+
+        jbtNum1.addActionListener(new ListenToOne());
+        jbtNum2.addActionListener(new ListenToTwo());
+        jbtNum3.addActionListener(new ListenToThree());
+        jbtNum4.addActionListener(new ListenToFour());
+        jbtNum5.addActionListener(new ListenToFive());
+        jbtNum6.addActionListener(new ListenToSix());
+        jbtNum7.addActionListener(new ListenToSeven());
+        jbtNum8.addActionListener(new ListenToEight());
+        jbtNum9.addActionListener(new ListenToNine());
+        jbtNum0.addActionListener(new ListenToZero());
+
+        jbtAdd.addActionListener(new ListenToAdd());
+        jbtSubtract.addActionListener(new ListenToSubtract());
+        jbtMultiply.addActionListener(new ListenToMultiply());
+        jbtDivide.addActionListener(new ListenToDivide());
+        jbtSolve.addActionListener(new ListenToSolve());
+        jbtClear.addActionListener(new ListenToClear());
+    } //JavaCaluclator()
+
+    class ListenToClear implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            //display = jtfResult.getText();
+            jtfResult.setText("");
+            addBool = false;
+            subBool = false;
+            mulBool = false;
+            divBool = false;
+
+            TEMP = 0;
+            SolveTEMP = 0;
+        }
     }
-    if (operation == '-')
-    {
-        System.out.println("your answer is " + (num1 - num2));
+
+    class ListenToOne implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            display = jtfResult.getText();
+            jtfResult.setText(display + "1");
+        }
     }
-    if (operation == '/')
-    {
-        System.out.println("your answer is " + (num1 / num2));
+
+    class ListenToTwo implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            display = jtfResult.getText();
+            jtfResult.setText(display + "2");
+        }
     }
-    if (operation == '*')
-    {
-        System.out.println("your answer is " + (num1 * num2));
+
+    class ListenToThree implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            display = jtfResult.getText();
+            jtfResult.setText(display + "3");
+        }
     }
-    
+
+    class ListenToFour implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            display = jtfResult.getText();
+            jtfResult.setText(display + "4");
+        }
     }
-}
+
+    class ListenToFive implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            display = jtfResult.getText();
+            jtfResult.setText(display + "5");
+        }
+    }
+
+    class ListenToSix implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            display = jtfResult.getText();
+            jtfResult.setText(display + "6");
+        }
+    }
+
+    class ListenToSeven implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            display = jtfResult.getText();
+            jtfResult.setText(display + "7");
+        }
+    }
+
+    class ListenToEight implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            display = jtfResult.getText();
+            jtfResult.setText(display + "8");
+        }
+    }
+
+    class ListenToNine implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            display = jtfResult.getText();
+            jtfResult.setText(display + "9");
+        }
+    }
+
+    class ListenToZero implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            display = jtfResult.getText();
+            jtfResult.setText(display + "0");
+        }
+    }
+
+    class ListenToAdd implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            TEMP = Double.parseDouble(jtfResult.getText());
+            jtfResult.setText("");
+            addBool = true;
+        }
+    }
+
+    class ListenToSubtract implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            TEMP = Double.parseDouble(jtfResult.getText());
+            jtfResult.setText("");
+            subBool = true;
+        }
+    }
+
+    class ListenToMultiply implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            TEMP = Double.parseDouble(jtfResult.getText());
+            jtfResult.setText("");
+            mulBool = true;
+        }
+    }
+
+    class ListenToDivide implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            TEMP = Double.parseDouble(jtfResult.getText());
+            jtfResult.setText("");
+            divBool = true;
+        }
+    }
+
+    class ListenToSolve implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            SolveTEMP = Double.parseDouble(jtfResult.getText());
+            if (addBool == true)
+                SolveTEMP = SolveTEMP + TEMP;
+            else if ( subBool == true)
+                SolveTEMP = SolveTEMP - TEMP;
+            else if ( mulBool == true)
+                SolveTEMP = SolveTEMP * TEMP;
+            else if ( divBool == true)
+                            SolveTEMP = SolveTEMP / TEMP;
+            jtfResult.setText(  Double.toString(SolveTEMP));
+
+            addBool = false;
+            subBool = false;
+            mulBool = false;
+            divBool = false;
+        }
+    }
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        Java_Calculator_demo calc = new Java_Calculator_demo();
+        calc.pack();
+        calc.setLocationRelativeTo(null);
+                calc.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        calc.setVisible(true);
+    }
+
+} //JavaCalculator
